fc1cbf1d1a48b4f9a63eedb651a278af932d8584
Florian Wilkens <gh@1wilkens.org>
2017-12-10 19:49:33 +0100

    [libgreeter] Add RdmGreeterError and make RdmGreeter::new never panic

--- libgreeter/src/lib.rs
+++ libgreeter/src/lib.rs
@@ -11,9 +11,12 @@ extern crate rdmcommon;
 use slog::Logger;
 
 use futures::{Future, Stream, Sink};
+
 use tokio_core::reactor::{Core, Handle};
 use tokio_io::AsyncRead;
 use tokio_uds::UnixStream;
+
+use std::io;
 
 use rdmcommon::ipc;
 use rdmcommon::util;
@@ -25,11 +28,28 @@ pub struct RdmGreeter {
     log: Logger
 }
 
+pub enum RdmGreeterError {
+    Ipc(ipc::IpcError),
+    Io(io::Error),
+    FailedHandshake
+}
+
+impl From<io::Error> for RdmGreeterError {
+    fn from(err: io::Error) -> RdmGreeterError {
+        RdmGreeterError::Io(err)
+    }
+}
+
+impl From<ipc::IpcError> for RdmGreeterError {
+    fn from(err: ipc::IpcError) -> RdmGreeterError {
+        RdmGreeterError::Ipc(err)
+    }
+}
+
 impl RdmGreeter {
-    pub fn new<L : Into<Option<Logger>>>(logger: L) -> Result<RdmGreeter, ipc::IpcError> {
+    pub fn new<L : Into<Option<Logger>>>(logger: L) -> Result<RdmGreeter, RdmGreeterError> {
         let log = logger.into().unwrap_or(util::plain_logger());
-        let mut core = Core::new()
-            .expect("[rdmgreeter] Failed to instantiate new Core");
+        let mut core = Core::new()?;
         let handle = core.handle();
         // TODO: move this into private fn
         debug!(log, "[RdmGreeter::new] Connecting server socket");
@@ -38,17 +58,17 @@ impl RdmGreeter {
         debug!(log, "[RdmGreeter::new] Sending ClientHello");
         let tx = core.run(tx.send(ipc::IpcMessage::ClientHello))?;
         debug!(log, "[RdmGreeter::new] Reading server response");
-        let (resp, rx) = core.run(rx.take(1).into_future()).expect("[rdmgreeter] Failed to receive handshake");
+        let (resp, rx) = core.run(rx.take(1).into_future().map_err(|(err, _)| err))?;
 
         debug!(log, "[RdmGreeter::new] Got server response"; "response" => ?resp);
         match resp {
             Some(ipc::IpcMessage::ServerHello) => Ok(RdmGreeter {
-                                                core: core,
-                                                receiver: Box::new(rx),
-                                                sender: Box::new(tx),
-                                                log: log,
-                                            }),
-            _ => Err(ipc::IpcError::UnknownMessageType)
+                                                        core: core,
+                                                        receiver: Box::new(rx),
+                                                        sender: Box::new(tx),
+                                                        log: log,
+                                                  }),
+            _ => Err(RdmGreeterError::FailedHandshake)
         }
     }
 }
